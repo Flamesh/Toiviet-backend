11599d201eb4eaa24fc5239f02ef681f8c8cf0cf
tuyenkbhb@gmail.com <tuyenkbhb@gmail.com>
2020-08-10 16:50:11 +0700

    continues fix auth

--- routers/api/user.js
+++ routers/api/user.js
@@ -1,7 +1,24 @@
-const mongoose = require('mongoose')
-const router = require('express').Router()
-const passport= require('passport')
-const User = mongoose.model('User')
-const auth = require('../auth')
+const mongoose = require("mongoose");
+const router = require("express").Router();
+const passport = require("passport");
+const User = mongoose.model("User");
+const auth = require("../auth");
 
+router.get("/user", auth.require, function (req, res, next) {
+  User.findById(req.payload.id)
+  .then(function (user) {
+    if (!user) {
+      return res.sendStatus(401);
+    }
+    return res.json({ user: user.toAuthJSON() });
+  }).catch(next)
+});
 
+router.put('/user', auth.required, function(req, res, next) {
+    User.findById(res.payload.id).then(function(user){
+        if(!user) {return res.sendStatus(401)}
+    })
+
+    // only update fields that were actually passed...
+    
+})
